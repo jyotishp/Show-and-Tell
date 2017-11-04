711fbaca9e3b7a09966ab02e7d7a61de331b836d
Sai Teja Reddy Moolamalla <saiteja0317@gmail.com>
2017-11-04 22:21:17 +0530

    Update process_text.py

--- process_text.py
+++ process_text.py
@@ -1,20 +1,21 @@
-import string
-from sets import Set
-from collections import Counter
-import pickle
-
+with open("Flickr8k.token.txt") as textFile:
+    lines = [line.split('\t') for line in textFile]
+    
 class img():
     def __init__(self, name, cap):
         self.name = name
         self.cap = cap
 
+import string
+from sets import Set
+from collections import Counter
+import pickle
+
 images = []
 maxl = 0
+
 vocab = []
-    
-with open("Flickr8k.token.txt") as textFile:
-    lines = [line.split('\t') for line in textFile]
-    
+        
 for line in lines:
     fname = line[0].split("#")[0]
     caption = line[1].split("\n")[0].split(".")[0]
@@ -27,13 +28,26 @@ for line in lines:
     maxl = max(maxl, len(caption))
     i = img(fname, caption)
     images.append(i)
-
-vocab = Counter(vocab)
     
+vocab = Counter(vocab)
 keys = vocab.keys()
 
 token_to_id = {key:pos for pos, key in enumerate(keys)}
 id_to_token = {pos:key for pos, key in enumerate(keys)}    
-    
+
 pickle.dump(token_to_id, open('token_to_id.p', 'wb') )
-pickle.dump(id_to_token, open('id_to_token.p', 'wb') )    
+pickle.dump(id_to_token, open('id_to_token.p', 'wb') )
+
+f = open("train_captions.txt", 'w')
+for line in lines:
+    fname = line[0].split("#")[0]
+    caption = line[1].split("\n")[0].split(".")[0]
+    caption = caption.translate(None, string.punctuation).lower()
+    caption = caption.split(" ")  
+    caption.append("<end>")    
+    caption = ["<start>"] + caption
+    caption = list(filter(lambda a: a != "", caption))
+    num_caption = map(lambda a: str(token_to_id[a]), caption)
+    f.write('%'.join([fname]+num_caption))
+    f.write('\n')
+f.close()
